192cba9cf7f911987688548f46b95ed067ad0432
Roj <ez@roj.im>
2023-08-11 19:19:25 +0300

    Update test deps

--- js/_test_deps.ts
+++ js/_test_deps.ts
@@ -1,1 +1,5 @@
-export { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
+export {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.198.0/assert/mod.ts";
+export { describe, it } from "https://deno.land/std@0.198.0/testing/bdd.ts";
